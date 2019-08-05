a72a69b3c9fae5633486e592b46fbec08ddf3d8a
RyadForGamessss <53405623+RyadForGamessss@users.noreply.github.com>
2019-08-05 17:02:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,37 +14,37 @@ client.on('message', message => {
   if (message.author.id !== "488746365933846529") return;
 
   
-  if (message.content.startsWith(prefix + 'Sw')) {
+  if (message.content.startsWith(prefix + 'NotAnesSw')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
 
  
-  if (message.content.startsWith(prefix + 'Sl')) {
+  if (message.content.startsWith(prefix + 'NotAnesSl')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
 
 
-if (message.content.startsWith(prefix + 'setname')) {
+if (message.content.startsWith(prefix + 'NotAnesSn')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`:white_check_mark: ${argresult}`)
   return message.reply("You Can change the username 2 times per hour");
 } 
 
-if (message.content.startsWith(prefix + 'Sa')) {
+if (message.content.startsWith(prefix + 'NotAnesSa')) {
   client.user.setAvatar(argresult);
    message.channel.sendMessage(`:white_check_mark: ${argresult}`;
 }
 
-if (message.content.startsWith(prefix + 'St')) {
+if (message.content.startsWith(prefix + 'NotAnesSt')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
-if (message.content.startsWith(prefix + 'Pl')) {
+if (message.content.startsWith(prefix + 'NotAnesPl')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
